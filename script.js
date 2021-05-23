a1725f98ba04bd88b232f2cccbb81a8f57d7d50a
Allen <sambajon.allen.as@gmail.com>
2021-05-23 20:59:28 +1200

    fixed the names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById('regsitration-modal');
+var modal = document.getElementById('registration-modal');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
